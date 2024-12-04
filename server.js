a05d40c8f894ee6d9c1139dba72701b832f205f7
Prerak Madan <madanjatin2004@gmail.com>
2024-12-04 18:02:38 +0530

    add

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const cron = require('node-cron');
 const connectDB = require('./config/db');
 const taskController = require('./controllers/taskController');
 const Task = require('./models/Task');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose
+    .connect('mongodb://localhost:27017/tasks')  // Default connection string
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Start the server after successful connection
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Could not connect to MongoDB', err);
+    });
 
 dotenv.config();
 const app = express();
